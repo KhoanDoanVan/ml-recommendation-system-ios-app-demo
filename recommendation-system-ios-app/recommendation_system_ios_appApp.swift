848b9b5075f74823471f2f033dae960bc134cb7e
khoanisdev <doanvankhoan124@gmail.com>
2025-03-24 14:30:31 +0700

    Initial Commit

---
+++ recommendation-system-ios-app/recommendation_system_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recommendation_system_ios_appApp.swift
+//  recommendation-system-ios-app
+//
+//  Created by Đoàn Văn Khoan on 24/3/25.
+//
+
+import SwiftUI
+
+@main
+struct recommendation_system_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
